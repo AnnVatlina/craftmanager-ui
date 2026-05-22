553f12f767e67afb8e100ed444dbdca8a9c18bb1
AnnVatlina <AnnVatlina@users.noreply.github.com>
2026-05-22 18:56:44 +0000

    deploy: 6e728d1d2e50b054b906fd521b92b50c32e5a027

--- assets/auth-Bwn__uDE.js
+++
@@ -1,1 +0,0 @@
-import{r as e,m as t}from"./index-MSqEtKgS.js";const r=e({get isAuthenticated(){return!!localStorage.getItem("access_token")},logout(){localStorage.removeItem("access_token"),localStorage.removeItem("refresh_token"),t.push("/login")}});export{r as a};
