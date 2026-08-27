e53221ab8cfa9c604357c69383ccf75e32e6bebd
AnnVatlina <AnnVatlina@users.noreply.github.com>
2026-08-27 17:29:42 +0000

    deploy: c2f8174d02fbeafb50a3997441991195daa0555b

---
+++ assets/auth-E5PtNGhB.js
@@ -0,0 +1,1 @@
+import{e,B as t}from"./index-EpncmicG.js";const r=e({get isAuthenticated(){return!!localStorage.getItem("access_token")},logout(){localStorage.removeItem("access_token"),localStorage.removeItem("refresh_token"),t.push("/login")}});export{r as a};
