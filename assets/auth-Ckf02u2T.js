558f06fdcefc3259d21bd7074102fbc21ac76ed4
AnnVatlina <AnnVatlina@users.noreply.github.com>
2026-08-30 16:36:40 +0000

    deploy: cf93b7a59417bdcd3365f3867b2a92284ca3ee30

---
+++ assets/auth-Ckf02u2T.js
@@ -0,0 +1,1 @@
+import{e,B as t}from"./index-BSCbVhPW.js";const r=e({get isAuthenticated(){return!!localStorage.getItem("access_token")},logout(){localStorage.removeItem("access_token"),localStorage.removeItem("refresh_token"),t.push("/login")}});export{r as a};
